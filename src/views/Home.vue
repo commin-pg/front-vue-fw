fc56c483013732cb2819c797f219918f27eb4cde
khm0813 <khm.commin@gmail.com>
2021-12-06 15:44:08 +0900

    app -> home 변경

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,108 @@
-<template >
-    <div>
-       <h1>
-        Home Page
-       </h1>
-    </div>
+<template>
+  <div id="home">
+    <section class="todoapp">
+      <Header @insertTodo="insertTodo" />
+      <Todo
+        :todos="filteredList"
+        @removeTodo="removeTodo"
+        @updateDone="updateDone"
+        @updateTodo="updateTodo"
+      />
+      <Footer
+        :filterType="filterType"
+        :size="filteredList.length"
+        @onFilterType="handleFilterType"
+      />
+    </section>
+  </div>
 </template>
+
 <script>
+import "../assets/css/main.css";
+import Header from "../components/Header.vue";
+import Todo from "../components/Todo.vue";
+import Footer from "../components/Footer.vue";
+
 export default {
-    
-}
+  name: "Home",
+  components: {
+    Header,
+    Todo,
+    Footer,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: new Date(),
+          text: "Vue 공부하기",
+          isDone: true,
+        },
+        {
+          id: new Date() + 1,
+          text: "치킨 먹기",
+          isDone: false,
+        },
+      ],
+      filterType: "All",
+    };
+  },
+  methods: {
+    insertTodo(text) {
+      this.todos = [
+        ...this.todos,
+        {
+          id: new Date().getTime(),
+          text,
+          isDone: false,
+        },
+      ];
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    updateDone(id) {
+      const todos = [...this.todos];
+      const todo = todos.find((todo) => todo.id === id);
+
+      if (todo) {
+        todo.isDone = !todo.isDone;
+        this.todos = todos;
+      }
+    },
+    updateTodo({ id, text }) {
+      const todos = [...this.todos];
+      const todo = todos.find((todo) => todo.id === id);
+
+      if (todo) {
+        todo.text = text;
+        this.todos = todos;
+      }
+    },
+    handleFilterType(type) {
+      this.filterType = type;
+    },
+  },
+  computed: {
+    filteredList() {
+      switch (this.filterType) {
+        case "All": {
+          return this.todos;
+        }
+        case "Active": {
+          return this.todos.filter((todo) => !todo.isDone);
+        }
+        case "Completed": {
+          return this.todos.filter((todo) => todo.isDone);
+        }
+        default: {
+          return [];
+        }
+      }
+    },
+  },
+};
 </script>
-<style >
-    
+
+<style>
 </style>
